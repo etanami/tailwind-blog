f1071ba386645c4007deeda5e89f3ac33be12b05
Etanami Olatunji <etanamiolatunji@gmail.com>
2021-05-14 13:03:42 +0100

    add: custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['Sansita', 'sans-serif'],
+        body: ['Lato', 'sans-serif'],
+      },
+      colors: {
+        pri: '#081F4D',
+        sec: '#0083FF',
+        gen_text: '#3B4D71',
+        bg_text: '#D9E6FF',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
